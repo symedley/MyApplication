6ae41132654d52db6218166cfed82e9a1500fd05
symedley <symedley@gmail.com>
2018-01-30 13:33:18 -0800

    My practice app for beginner fragment stuff, with interaction between fragment and activity. Look at whole file history (not sure if that's available since I am just createing the Git repo now) to see different ways of hooking them together.

---
+++ src/main/java/com/example/susannah/myapplication/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.susannah.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.util.logging.Logger;
+
+public class MainActivity extends AppCompatActivity
+implements OneFragment.OnFragmentInteractionListener {
+    private static final String LOG_TAG="MainActivity";
+
+    public void onFragmentInteraction() {
+        Log.v(LOG_TAG, "onFragment interaction in MainActivity");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (findViewById(R.id.fragment_container) != null) {
+            if (savedInstanceState != null) {
+                return;
+            }
+            OneFragment oneFragment = new OneFragment();
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, oneFragment).commit();
+        }
+    }
+}
