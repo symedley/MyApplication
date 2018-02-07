77a817711e791ffe671875e6fe4e3cc0ebb10bfa
symedley <symedley@gmail.com>
2018-02-07 09:47:34 -0800

    Support for vertical: open a new Activity on button press and show the passed in string in the new activity. Reuse the TwoFragment. This builds and runs. But the xml for the new two_holder_activity is way too complicated. Should be able to just use a single FrameLayout.

--- src/main/java/com/example/susannah/myapplication/MainActivity.java
+++ src/main/java/com/example/susannah/myapplication/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.susannah.myapplication;
 
+import android.app.FragmentManager;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,13 +13,20 @@ import java.util.logging.Logger;
 public class MainActivity extends AppCompatActivity
     implements OneFragment.OnFragmentInteractionListener{
     private static final String LOG_TAG="MainActivity";
-
-    private OneFragment oneFragment;
-    private TwoFragment twoFragment;
+    private Boolean isLandscape;
+    private static final String EXTRA_MESSAGE = "com.example.susannah.myapplication.EXTRA_MESSAGE";
+    protected OneFragment oneFragment;
+    protected TwoFragment twoFragment;
 
     public void onFragmentInteraction(String newString) {
         Log.v(LOG_TAG, "onFragment interaction in MainActivity");
-        twoFragment.updateMessageView("NEW NEW: " + newString);
+        if (isLandscape == true) {
+            twoFragment.updateMessageView("NEW NEW: " + newString);
+        }else {
+            Intent intent = new Intent(this, TwoHolderActivity.class);
+            intent.putExtra(EXTRA_MESSAGE, newString);
+            startActivity(intent);
+        }
     }
 
     @Override
@@ -25,12 +34,14 @@ public class MainActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (findViewById(R.id.fragment_container) != null) {
+            isLandscape = false ;
             if (savedInstanceState != null) {
                 return;
             } // Not dealing properly with savedInstanceState
             oneFragment = new OneFragment();
             getSupportFragmentManager().beginTransaction().add(R.id.fragment_container, oneFragment).commit();
         } else {
+            isLandscape = true;
             oneFragment = new OneFragment();
             getSupportFragmentManager().beginTransaction().add(R.id.fragment_left_frame, oneFragment).commit();
             twoFragment = TwoFragment.newInstance(getString(R.string.no_message_yet));
